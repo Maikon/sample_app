0da9f70dfe0dfe55d54427a8de4706487ece63be
Makis <makisotman@gmail.com>
2013-10-21 12:37:47 +0100

    Exercises chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,7 +16,8 @@ describe "Authentication" do
   	before { visit signin_path }
 
   	describe "with invalid information" do
-  		before { click_button "Sign in" }
+  		let(:user) { FactoryGirl.create(:user) }
+      before { click_button "Sign in" }
 
   		it { should have_title('Sign in') }
   		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
@@ -25,6 +26,12 @@ describe "Authentication" do
   			before { click_link "Home" }
   			it { should_not have_selector('div.alert.alert-error') }
   		end
+
+      it { should_not have_title(user.name) }
+      it { should_not have_link('Users', href: users_path) }
+      it { should_not have_link('Profile', href: user_path(user)) }
+      it { should_not have_link('Settings', href: edit_user_path(user)) }
+      it { should_not have_link('Sign out', href: signout_path) }
   	end
 
   	describe "with valid information" do 
@@ -64,6 +71,20 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
